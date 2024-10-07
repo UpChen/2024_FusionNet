3f3e62fd27831fda6008b2fc60518255c22ff654
Mingchen Xu <675220387@qq.com>
2024-10-07 11:22:39 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,5 @@
 VMD_training_root = ('/home/xum35/Desktop/File/Dataset/Mirror/VMD_reorganize/train', 'video', 'VMD_train')
 VMD_test_root = ('/home/xum35/Desktop/File/Dataset/Mirror/VMD_reorganize/test', 'video', 'VMD_test')
 
-# VMD_training_root = ('/home/xum35/Desktop/File/Dataset/VMD_reorganize(copy)/train', 'video', 'VMD_train')
-# VMD_test_root = ('/home/xum35/Desktop/File/Dataset/VMD_reorganize(copy)/test', 'video', 'VMD_test')
-
-# VMD_training_root = ('/home/xum35/Desktop/File/Dataset/FMIV_easy_resplit/train', 'video', 'VMD_train')
-# VMD_test_root = ('/home/xum35/Desktop/File/Dataset/FMIV_easy_resplit/test', 'video', 'VMD_test')
-
-# VMD_training_root = ('/home/xum35/Desktop/File/Dataset/FMIV_easy_resplit(copy)/train', 'video', 'VMD_train')
-# VMD_test_root = ('/home/xum35/Desktop/File/Dataset/FMIV_easy_resplit(copy)/test', 'video', 'VMD_test')
-
-# VMD_training_root = ('/scratch/c.c2098753/Dataset/VMD_reorganize/train', 'video', 'VMD_train')
-# VMD_test_root = ('/scratch/c.c2098753/Dataset/VMD_reorganize/test', 'video', 'VMD_test')
-
-# VMD_training_root = ('/scratch/c.c2098753/Dataset/VMD_reorganize(copy)/train', 'video', 'VMD_train')
-# VMD_test_root = ('/scratch/c.c2098753/Dataset/VMD_reorganize(copy)/test', 'video', 'VMD_test')
-
 
 
